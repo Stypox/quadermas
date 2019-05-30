2cc47ae3215e407eb74506671ba2436bd89bbd48
Stypox <stypox@pm.me>
2019-05-30 23:06:26 +0200

    Add Subject view

--- android/app/src/main/java/com/stypox/mastercom_workbook/Subject.java
+++ android/app/src/main/java/com/stypox/mastercom_workbook/Subject.java
@@ -3,16 +3,40 @@ package com.stypox.mastercom_workbook;
 import android.content.Context;
 import android.support.constraint.ConstraintLayout;
 import android.view.LayoutInflater;
+import android.widget.TextView;
 
-public class SubjectView extends ConstraintLayout {
-    SubjectView(Context context) {
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class Subject extends ConstraintLayout {
+    private String name;
+    private String id;
+
+    private TextView nameView;
+    private TextView teacherView;
+
+    Subject(Context context, JSONObject data) throws JSONException {
         super(context);
-        inflate(context);
+
+        this.name = data.getString("nome");
+        this.id = data.getString("id");
+
+        initializeViews(context);
     }
 
-    private void inflate(Context context) {
+    private void initializeViews(Context context) {
         LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         inflater.inflate(R.layout.subject, this);
         onFinishInflate();
+    }
+
+    @Override
+    protected void onFinishInflate() {
+        super.onFinishInflate();
+
+        nameView = findViewById(R.id.name);
+        teacherView = findViewById(R.id.teacher);
+
+        nameView.setText(name);
     }
 }
