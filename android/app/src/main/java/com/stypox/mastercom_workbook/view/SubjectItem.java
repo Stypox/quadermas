866b29e5ce491cf7544b89b59469091400233d70
Stypox <stypox@pm.me>
2019-06-06 12:30:51 +0200

    Show average in SubjectItem with color

--- android/app/src/main/java/com/stypox/mastercom_workbook/view/SubjectItem.java
+++ android/app/src/main/java/com/stypox/mastercom_workbook/view/SubjectItem.java
@@ -3,6 +3,7 @@ package com.stypox.mastercom_workbook.view;
 import android.content.Context;
 import android.content.Intent;
 import android.support.constraint.ConstraintLayout;
+import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.TextView;
@@ -11,13 +12,15 @@ import com.stypox.mastercom_workbook.R;
 import com.stypox.mastercom_workbook.data.MarkData;
 import com.stypox.mastercom_workbook.data.SubjectData;
 import com.stypox.mastercom_workbook.extractor.Extractor;
+import com.stypox.mastercom_workbook.util.MarkFormatting;
 
 import java.util.ArrayList;
 
 public class SubjectItem extends ConstraintLayout implements View.OnClickListener {
     private SubjectData data;
 
-    private TextView teacherView;
+    private TextView teacherTextView;
+    private TextView averageTextView;
 
     public SubjectItem(Context context, SubjectData data) {
         super(context);
@@ -36,21 +39,42 @@ public class SubjectItem extends ConstraintLayout implements View.OnClickListene
         super.onFinishInflate();
 
         TextView nameView = findViewById(R.id.name);
-        teacherView = findViewById(R.id.teacher);
+        teacherTextView = findViewById(R.id.teacher);
+        averageTextView = findViewById(R.id.average);
 
         nameView.setText(data.getName());
     }
 
     public void onMarksLoaded(ArrayList<MarkData> marks) {
         if (marks.isEmpty()) {
-            this.teacherView.setText(getContext().getString(R.string.error_no_marks));
+            this.teacherTextView.setText(getContext().getString(R.string.error_no_marks));
         } else {
-            this.teacherView.setText(data.getTeacher());
+            this.teacherTextView.setText(data.getTeacher());
             setOnClickListener(this);
         }
+        showAverage();
     }
     public void onMarksLoadingError(Extractor.Error error) {
-        this.teacherView.setText(error.toString(getContext()));
+        this.teacherTextView.setText(error.toString(getContext()));
+        showAverage();
+    }
+
+    public void showAverage() {
+        try {
+            float average = data.getAverage(data.getMarks().get(0).getTerm()); // current average
+            averageTextView.setText(MarkFormatting.floatToString(average, 3));
+
+            if (average < 6) {
+                averageTextView.setTextColor(getResources().getColor(R.color.failingMark));
+            } else if (average < 8) {
+                averageTextView.setTextColor(getResources().getColor(R.color.halfwayMark));
+            } else {
+                averageTextView.setTextColor(getResources().getColor(R.color.excellentMark));
+            }
+        } catch (Throwable e) {
+            averageTextView.setText("?");
+            averageTextView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 50);
+        }
     }
 
     @Override
