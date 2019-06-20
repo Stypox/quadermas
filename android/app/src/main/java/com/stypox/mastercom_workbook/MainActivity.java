0a465dc7531c657dc9208393bcf56e1456dbd2ce
Stypox <stypox@pm.me>
2019-06-20 14:49:55 +0200

    Remove useless "settings" button in the upper bar of main activity

--- android/app/src/main/java/com/stypox/mastercom_workbook/MainActivity.java
+++ android/app/src/main/java/com/stypox/mastercom_workbook/MainActivity.java
@@ -277,20 +277,6 @@ public class MainActivity extends AppCompatActivity
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
