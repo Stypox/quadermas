6961e60f7447e64add7d34e0a786e39d60e7939e
Stypox <stypox@pm.me>
2019-10-29 21:19:01 +0100

    Add Date to DocumentData to sort documents by date

--- app/src/main/java/com/stypox/mastercom_workbook/data/DocumentData.java
+++ app/src/main/java/com/stypox/mastercom_workbook/data/DocumentData.java
@@ -3,18 +3,24 @@ package com.stypox.mastercom_workbook.data;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class DocumentData {
     private final String name;
     private final String id;
     private final String mime;
     private final String owner;
+    private final Date date;
     private final String subject;
 
-    public DocumentData(JSONObject json) throws JSONException {
+    public DocumentData(JSONObject json) throws JSONException, ParseException {
         name = json.getString("name");
         id = json.getString("id");
         mime = json.getString("mime");
         owner = json.getString("owner_name") + " " + json.getString("owner_surname");
+        date = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ").parse(json.getString("received"));
 
         String quotedSubject = json.getJSONArray("tags").getString(0);
         subject = quotedSubject.substring(1, quotedSubject.length()-1);
@@ -31,6 +37,9 @@ public class DocumentData {
     }
     public String getOwner() {
         return owner;
+    }
+    public Date getDate() {
+        return date;
     }
     public String getSubject() {
         return subject;
