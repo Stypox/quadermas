d75d65e484d013c0b5c47cf7173a6b7daad750c6
Stypox <stypox@pm.me>
2020-04-25 15:25:27 +0200

    Add entry to change theme in settings

--- app/src/main/java/com/stypox/mastercom_workbook/settings/SettingsFragment.java
+++ app/src/main/java/com/stypox/mastercom_workbook/settings/SettingsFragment.java
@@ -14,11 +14,17 @@ public class SettingsFragment extends PreferenceFragmentCompat {
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         addPreferencesFromResource(R.xml.settings);
 
+        Preference theme = findPreference(getString(R.string.key_theme));
         Preference sourceCode = findPreference(getString(R.string.key_source_code));
         Preference reportBug = findPreference(getString(R.string.key_report_bug));
+        assert theme != null;
         assert sourceCode != null;
         assert reportBug != null;
 
+        theme.setOnPreferenceChangeListener((preference, newValue) -> {
+            requireActivity().recreate();
+            return true;
+        });
         sourceCode.setOnPreferenceClickListener(preference -> {
             openUrlInBrowser(requireContext(), getString(R.string.settings_source_code_url));
             return true;
