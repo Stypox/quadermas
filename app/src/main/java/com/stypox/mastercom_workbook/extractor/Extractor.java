8620d5c473c7ea449e3fec153c320e0ba86820b1
Stypox <stypox@pm.me>
2020-04-28 15:07:01 +0200

    Placeholders for subjects

--- app/src/main/java/com/stypox/mastercom_workbook/extractor/Extractor.java
+++ app/src/main/java/com/stypox/mastercom_workbook/extractor/Extractor.java
@@ -4,8 +4,15 @@ import android.os.Handler;
 import android.os.Looper;
 
 import com.stypox.mastercom_workbook.data.ClassData;
+import com.stypox.mastercom_workbook.data.MarkData;
 import com.stypox.mastercom_workbook.data.SubjectData;
+import com.stypox.mastercom_workbook.data.TopicData;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.text.ParseException;
+import java.util.ArrayList;
 import java.util.List;
 
 import io.reactivex.android.schedulers.AndroidSchedulers;
@@ -30,7 +37,49 @@ public class Extractor {
     private static String password;
 
     // extracted data
-    private static List<SubjectData> subjects;
+    private static List<SubjectData> subjects = new ArrayList<SubjectData>() {{
+        try {
+            add(new SubjectData(new JSONObject("{\"nome\": \"Latin language and literature\", \"id\": \"00100101\"}")) {{
+                setMarks(new ArrayList<MarkData>() {{
+                    add(new MarkData(new JSONObject("{\"valore\": \"7.25\", \"tipo\": \"Scritto\", \"data\": \"Tue, 28 Mar 2020 00:00:00 +0000\", \"note\": \"Test 3: translation\", \"docente\": \"John Smith\"}")));
+                    add(new MarkData(new JSONObject("{\"valore\": \"8.5\", \"tipo\": \"Orale\", \"data\": \"Wed, 22 Mar 2020 00:00:00 +0000\", \"note\": \"Oral test on 3 people on lupus, lupi, lupo\", \"docente\": \"John Smith\"}")));
+                    add(new MarkData(new JSONObject("{\"valore\": \"7\", \"tipo\": \"Scritto\", \"data\": \"Mon, 23 Dec 2019 00:00:00 +0000\", \"note\": \"Test 2: lupus, lupi, lupo\", \"docente\": \"John Smith\"}")));
+                    add(new MarkData(new JSONObject("{\"valore\": \"8.75\", \"tipo\": \"Orale\", \"data\": \"Fri, 6 Dec 2019 00:00:00 +0000\", \"note\": \"Oral test on 5 people on rosa, rosae, rosae\", \"docente\": \"John Smith\"}")));
+                    add(new MarkData(new JSONObject("{\"valore\": \"8.25\", \"tipo\": \"Pratico\", \"data\": \"Sat, 9 Nov 2019 00:00:00 +0000\", \"note\": \"Test 1 (practical): rosa, rosae, rosae\", \"docente\": \"John Smith\"}")));
+                }});
+                setTopics(new ArrayList<TopicData>() {{
+                    add(new TopicData(new JSONObject("{\"data\":\"Tue, 21 Mar 2020 00:00:00 +0000\", \"docente\":\"John Smith\", \"modulo\":\"Basics of latin\", \"descrizione\":\"Neutral form of lupus, lupi, lupo\", \"assegnazioni\":\"Study page 17\"}")));
+                    add(new TopicData(new JSONObject("{\"data\":\"Tue, 14 Mar 2020 00:00:00 +0000\", \"docente\":\"John Smith\", \"modulo\":\"Basics of latin\", \"descrizione\":\"Plural form of lupus, lupi, lupo\", \"assegnazioni\":\"Translate the text on page 23\"}")));
+                    add(new TopicData(new JSONObject("{\"data\":\"Wed, 8 Mar 2020 00:00:00 +0000\", \"docente\":\"John Smith\", \"modulo\":\"Discussion\", \"descrizione\":\"Answer to the following question: why is the latin phrase \\\"Lorem ipsum dolor sit amet, consectetur adipisci elit [...]\\\" used in computer programming? \", \"assegnazioni\":\"\"}")));
+                    add(new TopicData(new JSONObject("{\"data\":\"Tue, 7 Mar 2020 00:00:00 +0000\", \"docente\":\"John Smith\", \"modulo\":\"Basics of latin\", \"descrizione\":\"Singular form of lupus, lupi, lupo\", \"assegnazioni\":\"Study pages 14-15\"}")));
+                    add(new TopicData(new JSONObject("{\"data\":\"Wed, 1 Mar 2020 00:00:00 +0000\", \"docente\":\"John Smith\", \"modulo\":\"Discussion\", \"descrizione\":\"Discussion about how to study\", \"assegnazioni\":\"\"}")));
+                }});
+            }});
+            add(new SubjectData(new JSONObject("{\"nome\": \"Italian language\", \"id\": \"00100102\"}")) {{
+                setMarks(new ArrayList<MarkData>() {{
+                    add(new MarkData(new JSONObject("{\"valore\": \"9.5\", \"tipo\": \"Scritto\", \"data\": \"Wed, 29 Mar 2020 00:00:00 +0000\", \"note\": \"Vocabulary test about countries\", \"docente\": \"Rachel Jonas\"}")));
+                    add(new MarkData(new JSONObject("{\"valore\": \"8.75\", \"tipo\": \"Orale\", \"data\": \"Mon, 20 Mar 2020 00:00:00 +0000\", \"note\": \"Speaking test\", \"docente\": \"Rachel Jonas\"}")));
+                }});
+                setTopics(new ArrayList<TopicData>() {{
+                    add(new TopicData(new JSONObject("{\"data\":\"Mon, 27 Mar 2020 00:00:00 +0000\", \"docente\":\"Rachel Jonas\", \"modulo\":\"Vocabulary\", \"descrizione\":\"andare, passeggiare, camminare\", \"assegnazioni\":\"Study all of the words in table 7.2\"}")));
+                    add(new TopicData(new JSONObject("{\"data\":\"Tue, 21 Mar 2020 00:00:00 +0000\", \"docente\":\"Rachel Jonas\", \"modulo\":\"Vocabulary\", \"descrizione\":\"tastiera, schermo\", \"assegnazioni\":\"Read poem on page 139\"}")));
+                }});
+            }});
+            add(new SubjectData(new JSONObject("{\"nome\": \"Arithmetic and geometry\", \"id\": \"00100102\"}")) {{
+                setMarks(new ArrayList<MarkData>() {{
+                    add(new MarkData(new JSONObject("{\"valore\": \"5.5\", \"tipo\": \"Orale\", \"data\": \"Sat, 25 Mar 2020 00:00:00 +0000\", \"note\": \"Trigonometric functions: sin, cos, tan\", \"docente\": \"Mario Rossi\"}")));
+                }});
+                setTopics(new ArrayList<TopicData>() {{
+                    add(new TopicData(new JSONObject("{\"data\":\"Mon, 20 Mar 2020 00:00:00 +0000\", \"docente\":\"Mario Rossi\", \"modulo\":\"Trigonometry\", \"descrizione\":\"Inverse of trigonometric functions: asin, acos, atan\", \"assegnazioni\":\"On page 302 do exercises 16-17-19-24\"}")));
+                    add(new TopicData(new JSONObject("{\"data\":\"Sat, 18 Mar 2020 00:00:00 +0000\", \"docente\":\"Mario Rossi\", \"modulo\":\"Trigonometry\", \"descrizione\":\"sin, cos, tan\", \"assegnazioni\":\"Read and study pages 267-268\"}")));
+                }});
+            }});
+        } catch (JSONException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+    }};
     private static List<ClassData> classes;
 
 
