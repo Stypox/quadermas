49d032776e9cd0c00eb8f46be7aa6f422aeafb01
Stypox <stypox@pm.me>
2021-03-23 16:11:14 +0100

    Add button to view changelog to settings

--- app/src/main/java/com/stypox/mastercom_workbook/settings/SettingsFragment.java
+++ app/src/main/java/com/stypox/mastercom_workbook/settings/SettingsFragment.java
@@ -5,21 +5,28 @@ import android.os.Bundle;
 import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
 
+import com.stypox.mastercom_workbook.BuildConfig;
 import com.stypox.mastercom_workbook.R;
 
 import static com.stypox.mastercom_workbook.util.ShareUtils.openUrlInBrowser;
 
 public class SettingsFragment extends PreferenceFragmentCompat {
     @Override
-    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+    public void onCreatePreferences(final Bundle savedInstanceState, final String rootKey) {
         addPreferencesFromResource(R.xml.settings);
 
-        Preference theme = findPreference(getString(R.string.key_theme));
-        Preference sourceCode = findPreference(getString(R.string.key_source_code));
-        Preference reportBug = findPreference(getString(R.string.key_report_bug));
+        final Preference theme = findPreference(getString(R.string.key_theme));
+        final Preference sourceCode = findPreference(getString(R.string.key_source_code));
+        final Preference reportBug = findPreference(getString(R.string.key_report_bug));
+        final Preference changelog = findPreference(getString(R.string.key_changelog));
+
         assert theme != null;
         assert sourceCode != null;
         assert reportBug != null;
+        assert changelog != null;
+
+        changelog.setTitle(getString(R.string.settings_changelog,
+                BuildConfig.VERSION_NAME, BuildConfig.VERSION_CODE));
 
         theme.setOnPreferenceChangeListener((preference, newValue) -> {
             requireActivity().recreate();
@@ -31,6 +38,10 @@ public class SettingsFragment extends PreferenceFragmentCompat {
         });
         reportBug.setOnPreferenceClickListener(preference -> {
             openUrlInBrowser(requireContext(), getString(R.string.settings_report_bug_url));
+            return true;
+        });
+        changelog.setOnPreferenceClickListener(preference -> {
+            openUrlInBrowser(requireContext(), getString(R.string.settings_changelog_url));
             return true;
         });
     }
