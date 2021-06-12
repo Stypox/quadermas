8cad771f18560a7538fc6fa94d5c448c82d26b5a
Stypox <stypox@pm.me>
2021-06-12 15:02:41 +0200

    Click on timetable event to add to calendar

--- app/src/main/java/com/stypox/mastercom_workbook/view/holder/TimetableEventItemHolder.java
+++ app/src/main/java/com/stypox/mastercom_workbook/view/holder/TimetableEventItemHolder.java
@@ -2,6 +2,7 @@ package com.stypox.mastercom_workbook.view.holder;
 
 import android.text.format.DateFormat;
 import android.view.View;
+import android.widget.HorizontalScrollView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -10,6 +11,8 @@ import androidx.annotation.Nullable;
 import com.stypox.mastercom_workbook.R;
 import com.stypox.mastercom_workbook.data.DocumentData;
 import com.stypox.mastercom_workbook.data.TimetableEventData;
+import com.stypox.mastercom_workbook.util.HorizontalScrollViewTouchListener;
+import com.stypox.mastercom_workbook.util.ShareUtils;
 
 public class TimetableEventItemHolder extends ItemHolder<TimetableEventData> {
 
@@ -26,6 +29,10 @@ public class TimetableEventItemHolder extends ItemHolder<TimetableEventData> {
         teacherView = itemView.findViewById(R.id.teacher);
         beginTimeView = itemView.findViewById(R.id.beginTime);
         endTimeView = itemView.findViewById(R.id.endTime);
+
+        final HorizontalScrollView subjectScrollView
+                = itemView.findViewById(R.id.subjectScrollView);
+        subjectScrollView.setOnTouchListener(new HorizontalScrollViewTouchListener(itemView));
     }
 
     @Override
@@ -36,6 +43,11 @@ public class TimetableEventItemHolder extends ItemHolder<TimetableEventData> {
         final java.text.DateFormat timeFormat = DateFormat.getTimeFormat(context);
         beginTimeView.setText(timeFormat.format(data.getBegin()));
         endTimeView.setText(timeFormat.format(data.getEnd()));
+
+        itemView.setOnClickListener(v -> {
+            ShareUtils.addEventToCalendar(context, data.getSubject(), null, data.getTeacher(),
+                    data.getBegin(), data.getEnd());
+        });
     }
 
     private static class Factory implements ItemHolderFactory<TimetableEventData> {
