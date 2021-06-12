d9591a69903b62af83def91ba80ecf907604217f
Stypox <stypox@pm.me>
2021-06-12 14:20:09 +0200

    Make sure openUrlInBrowser() doesn't crash

--- app/src/main/java/com/stypox/mastercom_workbook/util/ShareUtils.java
+++ app/src/main/java/com/stypox/mastercom_workbook/util/ShareUtils.java
@@ -1,12 +1,17 @@
 package com.stypox.mastercom_workbook.util;
 
+import android.content.ActivityNotFoundException;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 
 public class ShareUtils {
-    public static void openUrlInBrowser(Context context, String url) {
-        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
-        context.startActivity(intent);
+    public static void openUrlInBrowser(final Context context, final String url) {
+        final Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+        try {
+            context.startActivity(intent);
+        } catch (final ActivityNotFoundException e) {
+            // ignore
+        }
     }
 }
